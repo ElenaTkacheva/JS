f28c81537a94755942eb235a69fbb07795081773
Elena <79642915+ElenaTkacheva@users.noreply.github.com>
2022-01-21 15:49:35 -0500

    Add files via upload

---
+++ Промостраница IPhone 13/scripts/scroll.js
@@ -0,0 +1,30 @@
+const links = document.querySelectorAll(".header-menu__item a");
+
+seamless.polyfill(); // метод для скролла в сафари
+
+links.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const id = element.getAttribute('href').substring(1);
+        const section = document.getElementById(id);
+        
+        if(section) {
+            seamless.elementScrollIntoView(section, {
+              behavior: "smooth",
+              block: "start",
+            });
+        } else {
+            seamless.elementScrollIntoView(
+              document.querySelector("#characteristics"),
+              {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+              }
+            );
+        }
+        
+    }) 
+})
+
